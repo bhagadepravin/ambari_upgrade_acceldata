dd0450b199e06880d44f748114b2f3f2d0a38317
Pravin Bhagade <pravin@acceldata.io>
2024-05-23 09:35:43 +0530

    Update generate_sql_backup_restore.sh

--- generate_sql_backup_restore.sh
+++ generate_sql_backup_restore.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
 # Variables
-MPACKS=("ambari-impala-mpack" "httpfs-ambari-mpack" "hue-ambari.mpack" "nifi-ambari-mpack" "spark3-ambari-3.2.2.mpack" "spark3-ambari-3.2.2-3.2.2.0-1.mpack")
-SERVICES=("IMPALA" "HTTPFS" "HUE" "NIFI" "SPARK3" "SPARK3")
+MPACKS=("ambari-impala-mpack" "httpfs-ambari-mpack" "hue-ambari.mpack" "spark3-ambari-3.2.2.mpack" "spark3-ambari-3.2.2-3.2.2.0-1.mpack")
+SERVICES=("IMPALA" "HTTPFS" "HUE" "SPARK3" "SPARK3")
+EXTRA_SERVICES=("RANGER" "RANGER_KMS" "KAFKA")
 BACKUP_DIR="pre_upgrade"
 RESTORE_DIR="post_upgrade"
 DB_NAME="service_conf_bck"
@@ -33,8 +34,10 @@ create_directories() {
 # Function to create backup SQL
 create_backup_sql() {
     BACKUP_SQL="$BACKUP_DIR/backup_configs.sql"
-    echo "create database if not exists $DB_NAME;" > $BACKUP_SQL
-    echo "use $DB_NAME;" >> $BACKUP_SQL
+    {
+        echo "create database if not exists $DB_NAME;"
+        echo "use $DB_NAME;"
+    } > "$BACKUP_SQL"
 }
 
 # Function to create restore SQL for a service
@@ -42,17 +45,44 @@ create_restore_sql() {
     local SERVICE=$1
     RESTORE_SQL="$RESTORE_DIR/restore_${SERVICE}.sql"
     LATEST_VERSION="(SELECT MAX(version) FROM $DB_NAME.configs_prior_patch WHERE service_name = '$SERVICE')"
-    echo "use ambari;" > $RESTORE_SQL
-    echo "update clusterconfig a set config_data = (select config_data from $DB_NAME.configs_prior_patch b where a.type_name = b.type_name and version = $LATEST_VERSION and service_name = '$SERVICE')," >> $RESTORE_SQL
-    echo "config_attributes = (select config_attributes from $DB_NAME.configs_prior_patch b where a.type_name = b.type_name and version = $LATEST_VERSION and service_name = '$SERVICE')" >> $RESTORE_SQL
-    echo "where config_id in (select config_id from serviceconfigmapping where service_config_id in (select service_config_id from serviceconfig where service_name = '$SERVICE' and version = (select max(version) from serviceconfig where service_name = '$SERVICE')));" >> $RESTORE_SQL
+    {
+        echo "use ambari;"
+        echo "update clusterconfig a set"
+        echo "    config_data = (select config_data from $DB_NAME.configs_prior_patch b where a.type_name = b.type_name and version = $LATEST_VERSION and service_name = '$SERVICE'),"
+        echo "    config_attributes = (select config_attributes from $DB_NAME.configs_prior_patch b where a.type_name = b.type_name and version = $LATEST_VERSION and service_name = '$SERVICE')"
+        echo "where"
+        echo "    config_id in (select config_id from serviceconfigmapping where service_config_id in (select service_config_id from serviceconfig where service_name = '$SERVICE' and version = (select max(version) from serviceconfig where service_name = '$SERVICE')));"
+    } > "$RESTORE_SQL"
 }
 
 # Function to append service names to backup SQL
 append_service_names_to_backup_sql() {
     local INSTALLED_SERVICES=("$@")
     SERVICE_NAMES=$(IFS=,; echo "${INSTALLED_SERVICES[*]}")
-    echo "CREATE TABLE IF NOT EXISTS configs_prior_patch AS SELECT cc.config_id, cc.type_name, cc.config_data, cc.config_attributes, service_name, sc.version, sc.note, (SELECT MAX(version) FROM ambari.serviceconfig s WHERE s.service_name = sc.service_name) AS max_version FROM ambari.serviceconfig sc INNER JOIN ambari.serviceconfigmapping scm ON sc.service_config_id = scm.service_config_id INNER JOIN ambari.clusterconfig cc ON cc.config_id = scm.config_id WHERE sc.service_name IN ($SERVICE_NAMES);" >> $BACKUP_SQL
+    {
+        echo "CREATE TABLE IF NOT EXISTS configs_prior_patch AS"
+        echo "SELECT"
+        echo "    cc.config_id,"
+        echo "    cc.type_name,"
+        echo "    cc.config_data,"
+        echo "    cc.config_attributes,"
+        echo "    service_name,"
+        echo "    sc.version,"
+        echo "    sc.note,"
+        echo "    (SELECT MAX(version) FROM ambari.serviceconfig s WHERE s.service_name = sc.service_name) AS max_version"
+        echo "FROM"
+        echo "    ambari.serviceconfig sc"
+        echo "INNER JOIN"
+        echo "    ambari.serviceconfigmapping scm"
+        echo "ON"
+        echo "    sc.service_config_id = scm.service_config_id"
+        echo "INNER JOIN"
+        echo "    ambari.clusterconfig cc"
+        echo "ON"
+        echo "    cc.config_id = scm.config_id"
+        echo "WHERE"
+        echo "    sc.service_name IN ($SERVICE_NAMES);"
+    } >> "$BACKUP_SQL"
 }
 
 # Function to identify installed mpacks and map to services
@@ -68,6 +98,16 @@ identify_installed_services() {
             create_restore_sql "$SERVICE"
         fi
     done
+    
+    # Include extra services directly
+    for SERVICE in "${EXTRA_SERVICES[@]}"; do
+        if [[ ! " ${INSTALLED_SERVICES[*]} " =~ " '$SERVICE' " ]]; then
+            INSTALLED_SERVICES+=("'$SERVICE'")
+        fi
+        log "Adding extra service $SERVICE to backup and restore scripts."
+        create_restore_sql "$SERVICE"
+    done
+
     append_service_names_to_backup_sql "${INSTALLED_SERVICES[@]}"
 }
 
